9e9e887a7d548163de9d3a746303aaa6fb804f91
Abhishek Shukla <91975183+abhis699@users.noreply.github.com>
2023-07-31 13:52:09 +0530

    ini AGNi

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=MiNaZuki kernel by Amack
+kernel.string=Agni Kernel by Abhishek
 do.devicecheck=1
 do.modules=0
 do.systemless=1
